65b5682d519602ae638e5112c676ca88b5156014
Fabio Elías Rengifo García <fabiorengifog@gmail.com>
2023-10-06 19:26:59 +0200

    Base movimiento

---
+++ Assets/TileMovement/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 GetTilePosition()
+    {
+        return (Vector3.right*transform.position.x+Vector3.forward*transform.position.z);
+    }
+
+
+}
---
+++ Assets/TileMovement/TileDetector.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileDetector : MonoBehaviour
+{
+    public enum TileDetectorType
+    {
+        Left,
+        Right,
+        Forward,
+        Backward,
+        Floor
+    }
+
+    public TileDetectorType detectorType;
+    PlayerController player;
+
+    void Start()
+    {
+        player = transform.parent.parent.GetComponent<PlayerController>();//we know the parent has this script
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Tile tile = other.GetComponent<Tile>();
+        if (tile != null)
+        {
+            switch (detectorType)
+            {
+                case TileDetectorType.Left:
+                    player.SetLeftTile(tile);
+                    break;
+                case TileDetectorType.Right:
+                    player.SetRightTile(tile);
+                    break;
+                case TileDetectorType.Forward:
+                    player.SetForwardTile(tile);
+                    break;
+                case TileDetectorType.Backward:
+                    player.SetBackwardTile(tile);
+                    break;
+                case TileDetectorType.Floor:
+                    player.SetCurrentTile(tile);
+                    break;
+            }
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Tile tile = other.GetComponent<Tile>();
+        if (tile != null)
+        {
+            switch (detectorType)
+            {
+                case TileDetectorType.Left:
+                    player.SetLeftTile(null);
+                    break;
+                case TileDetectorType.Right:
+                    player.SetRightTile(null);
+                    break;
+                case TileDetectorType.Forward:
+                    player.SetForwardTile(null);
+                    break;
+                case TileDetectorType.Backward:
+                    player.SetBackwardTile(null);
+                    break;
+                case TileDetectorType.Floor:
+                    player.SetCurrentTile(null);
+                    break;
+            }
+
+        }
+    }
+
+}
