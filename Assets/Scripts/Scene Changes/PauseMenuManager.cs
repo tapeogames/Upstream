d1a3c4ec4a7902768bef89f3c60647a242fd57c7
Fabio Elías Rengifo García <fabiorengifog@gmail.com>
2023-10-22 20:28:30 +0200

    Medium interfaces

--- Assets/Scripts/Scene Changes/PauseMenuManager.cs
+++ Assets/Scripts/Scene Changes/PauseMenuManager.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class PauseMenuManager : MonoBehaviour
 {
+    [SerializeField] private AudioMixer audioMixer;
+    private void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
     public void Exit()
     {
         SceneManager.LoadScene(3);
     }
+
+    public void CambiarVolumen(float volumen)
+    {
+        audioMixer.SetFloat("Volumen", volumen);
+    }
+
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+    }
+
 }
