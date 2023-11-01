2d3055f2c9e1ad2f955f25083e722e6e0a0ca9fd
Luna <lunabouzon79@gmail.com>
2023-11-01 13:07:56 +0100

    Cambio de escena panel de victoria

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class WinCondition : MonoBehaviour
 {
-    public Canvas win;
 
-    private void Start()
-    {
-        win.enabled = false;
-    }
+    public GameObject victoria;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ForwardPlayer"))
         {
             Debug.Log("entra trigger");
-            win.enabled = true;
+            victoria.SetActive(true);
         }
     }
 
-    public void Quitar()
-    {
-        win.enabled = false;
-    }
 }
