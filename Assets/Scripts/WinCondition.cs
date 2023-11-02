41320775e5d1ab52b4c778da2ba8f80441557c43
Luna <lunabouzon79@gmail.com>
2023-11-02 21:03:32 +0100

    cambio victoria

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class WinCondition : MonoBehaviour
 {
-    public Canvas win;
+    public GameObject win;
 
-    private void Start()
-    {
-        win.enabled = false;
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ForwardPlayer"))
         {
             Debug.Log("entra trigger");
-            win.enabled = true;
+            win.SetActive(true);
         }
     }
 
-    public void Quitar()
-    {
-        win.enabled = false;
-    }
 }
