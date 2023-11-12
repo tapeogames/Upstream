0e0f66f6056159805c830ce8427a8f796f140475
Luna <lunabouzon79@gmail.com>
2023-11-12 20:36:54 +0100

    objeto solucionado

--- Assets/Scripts/Tiles/Flow.cs
+++ Assets/Scripts/Tiles/Flow.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Flow : Tile
 {
-    public Transform[] partnersPosition;
-    private int index = 0;
+    public Transform partnersPosition;
     Vector3 posY = new Vector3(0, 1, 0);
     private bool isMoving = false;
     private float moveSpeed = 5f;
@@ -28,24 +27,16 @@ public class Flow : Tile
     }
 
     private void MoveToNextPosition()
-    {
-        
-        if (index < partnersPosition.Length)
-        {
-            Vector3 targetPosition = partnersPosition[index].position + posY;
-            float step = moveSpeed * Time.deltaTime;
-            playerAux.transform.position = Vector3.MoveTowards(playerAux.transform.position, targetPosition, step);
+    {        
+        Vector3 targetPosition = partnersPosition.transform.position + posY;
+        float step = moveSpeed * Time.deltaTime;
+        playerAux.transform.position = Vector3.MoveTowards(playerAux.transform.position, targetPosition, step);
 
-            if (Vector3.Distance(playerAux.transform.position, targetPosition) < 0.001f)
-            {
-                Debug.Log(partnersPosition[index].transform.position);
-                isMoving = false;
-            }
-        }
-        else
+        if (Vector3.Distance(playerAux.transform.position, targetPosition) < 0.1f)
         {
+            Debug.Log(partnersPosition.transform.position);
             isMoving = false;
-        }       
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -54,22 +45,11 @@ public class Flow : Tile
         {
             playerAux = other.GetComponentInParent<PlayerController>();
             isMoving = true;
-            index++;
-            if (index >= partnersPosition.Length)
-            {
-                index = 0;
-            }
         }
-        if (other.CompareTag("ObjectDetectorCurrent") && !isMoving && !PlayerController.grabbing)
+        else if (other.CompareTag("ObjectDetectorCurrent") && !isMoving && !PlayerController.grabbing)
         {
-            PushableObjectController pushableObject = other.GetComponentInParent<PushableObjectController>();
             playerAux = other.GetComponentInParent<PushableObjectController>();
             isMoving = true;
-            index++;
-            if (index >= partnersPosition.Length)
-            {
-                index = 0;
-            }
         }
     }
 }
