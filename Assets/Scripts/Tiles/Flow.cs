b74eb5c4d442162a84973f2ce11eca7b1e726039
Luna <lunabouzon79@gmail.com>
2023-11-12 12:15:08 +0100

    movimiento sin repeticion

--- Assets/Scripts/Tiles/Flow.cs
+++ Assets/Scripts/Tiles/Flow.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Flow : Tile
 {
@@ -29,47 +27,26 @@ public class Flow : Tile
             Vector3 targetPosition = partnersPosition[index].position + posY;
             float step = moveSpeed * Time.deltaTime;
             playerAux.transform.position = Vector3.MoveTowards(playerAux.transform.position, targetPosition, step);
-            playerAux.enabled = false;
 
-            if (Vector3.SqrMagnitude(playerAux.transform.position - targetPosition) < 0.0001f)
+            if (Vector3.Distance(playerAux.transform.position, targetPosition) < 0.001f)
             {
-                index++;
+                Debug.Log(partnersPosition[index].transform.position);
+                isMoving = false;
             }
-        }
-        else
-        {
-            isMoving = false;
-            playerAux.enabled = true; 
-            index = 0;
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("PlayerDetectorCurrent"))
-        {
-            playerAux = other.GetComponentInParent<PlayerController>();
-            isMoving = true;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("PlayerDetectorCurrent"))
+        if (other.CompareTag("PlayerDetectorCurrent") && !isMoving)
         {
-            if (!isMoving)
+            playerAux = other.GetComponentInParent<PlayerController>();
+            isMoving = true;
+            index++;
+            if (index >= partnersPosition.Length)
             {
-                playerAux.enabled = true;
+                index = 0;
             }
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("PlayerDetectorCurrent"))
-        {
-            PlayerController player = other.GetComponentInParent<PlayerController>();
-            player.enabled = true;
         }
     }
 }
