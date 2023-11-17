99d4da32ecc11a8c8042b48ca3c30d9d5a82a605
Lucas Rubio <81293638+LucasWiggie@users.noreply.github.com>
2023-11-17 23:59:33 +0100

    Arreglos canvas (inacabados)
    Pruebas Build para itch.io, dan problemas algunos canvas. Solucionar!!

--- Assets/Scripts/Tiles/Whirlpool.cs
+++ Assets/Scripts/Tiles/Whirlpool.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Whirlpool : Tile
 {
