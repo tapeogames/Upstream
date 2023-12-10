a33e7f6909f4e2e8cb6a8694db33736cf44c6c06
Morfeo26 <82976203+JavierAlper@users.noreply.github.com>
2023-12-10 18:06:59 +0100

    Arreglos pantalla de preguntas

--- Assets/Scripts/Scene Changes/QuestionsMenuManager.cs
+++ Assets/Scripts/Scene Changes/QuestionsMenuManager.cs
@@ -1,22 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.U2D;
 using UnityEngine.UI;
 
 public class QuestionsMenuManager : MonoBehaviour
 {
     public TextMeshProUGUI age;
+    public Button continueButton;
+    public Button chico;
+    public Button chica;
+    public Button otro;
+    public Sprite pressed;
+    public Sprite normal;
+    string gender = "x";
     int value;
 
     public void Start()
     {
         value = 0;
+        continueButton.interactable = false;
         age.SetText(value.ToString());
+
     }
-   
-   
+    public void Update()
+    {
+        if(gender != "x")
+        {
+            continueButton.interactable = true;
+            Image imagenBotonChico = chico.GetComponent<Image>();
+            Image imagenBotonChica = chica.GetComponent<Image>();
+            Image imagenBotonOtro = otro.GetComponent<Image>();
+            
+            if (gender == "Chico")
+            {
+                
+                imagenBotonChico.sprite = pressed;
+                imagenBotonChica.sprite = normal;
+                imagenBotonOtro.sprite = normal;
+            }
+            else if(gender == "Chica")
+            {
+
+                
+                imagenBotonChico.sprite = normal;
+                imagenBotonOtro.sprite = normal;
+                imagenBotonChica.sprite = pressed;
+            }
+            else
+            {
+
+                imagenBotonChico.sprite = normal;
+                imagenBotonChica.sprite = normal;
+              
+                imagenBotonOtro.sprite = pressed;
+            }
+        }
+
+        
+    }
+
+    public void Male()
+    {
+        gender = "Chico";
+    }
+    public void Female()
+    {
+        gender = "Chica";
+    }
+    public void Other()
+    {
+        gender = "Otro";
+    }
+
     public void NextScene()
     {
         StartCoroutine(DelayedNextScene());
